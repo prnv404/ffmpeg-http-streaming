2abc926a390d76b4aefdb894c1a02c66b18eaa92
pranav <pranavofficial404@gmail.com>
2023-09-30 19:36:32 +0530

    some config update

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,18 @@ import { createServer } from "http";
 import { stat, createReadStream } from "fs";
 import { promisify } from "util";
 
-const fileInto = promisify(stat);
-
 const filename = "./video/framer.mp4";
 
+// convertiong to promise to use await syntax the stat callback
+const fileInto = promisify(stat);
+
 createServer(async (req, res) => {
+	//  status of the file
 	const { size } = await fileInto(filename);
+
 	const range = req.headers.range;
+	/* The `if (range)` condition checks if the `range` header is present in the request. The `range`
+    header specifies the range of bytes that the client wants to receive in the response. */
 	if (range) {
 		let [start, end] = range.replace("/bytes=/", "").split("-");
 		const startOF = parseInt(start, 10);
@@ -19,6 +24,7 @@ createServer(async (req, res) => {
 			"Content-Length": startOF - endOF + 1,
 			"Content-Type": "video/mp4"
 		});
+		/*  reading the video file specified by the `filename` variable. */
 		createReadStream(filename, { start: startOF, end: endOF }).pipe(res);
 	}
 	res.writeHead(200, {
